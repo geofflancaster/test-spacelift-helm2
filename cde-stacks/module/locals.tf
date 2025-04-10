32d906305b35c305bd8358f0a5e16bde5466c929
Geoff Lancaster <geofflancaster@pingidentity.com>
2025-04-09 22:53:02 -0500

    fixed mapping

--- cde-stacks/module/locals.tf
+++ cde-stacks/module/locals.tf
@@ -1,6 +1,6 @@
 locals {
   multi_region_stages = ["stage", "prod"]
-  cdes = [
+  cde_maps = [
     for tuple in setunion(
       setproduct(toset(var.stages), toset([var.primary_region])),
       length(var.secondary_regions) > 0 ? setproduct(setintersection(local.multi_region_stages, toset(var.stages)), toset(var.secondary_regions)) : toset([])
@@ -9,4 +9,7 @@ locals {
       region = tuple[1]
     }
   ]
+  cdes = {
+    for cde_map in local.cde_maps : "${cde_map.stage}-${cde_map.region}" => cde_map
+  }
 }
