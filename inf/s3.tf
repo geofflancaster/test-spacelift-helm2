4f658eab3965c1996c0c0f71c144b0d5b1335be5
Geoff Lancaster <geofflancaster@pingidentity.com>
2025-04-30 15:03:31 -0700

    update s3

--- inf/s3.tf
+++ inf/s3.tf
@@ -1,9 +1,5 @@
 resource "aws_s3_bucket" "loki" {
   bucket_prefix = "loki-"
-}
+  force_destroy = true
 
-resource "aws_s3_bucket_acl" "loki" {
-  bucket = aws_s3_bucket.loki.id
-
-  acl = "private"
 }
